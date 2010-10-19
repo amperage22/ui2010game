6909e83edd75d7baaba031e2dc0855b2b04d00ec
alconis@gmail.com <alconis@gmail.com@b4ff2f4a-5cd7-df60-c775-ff6b566891b7>
2010-10-19 02:42:09 +0000

    Added monster class

---
+++ arrg/ARRG/monster.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ARRG_Game
+{
+  class monster
+  {
+
+    private int health, power;
+    private string name;
+
+    public monster(string name, int health, int power)
+    {
+      this.name = name;
+      this.health = health;
+      this.power = power;
+    }
+
+    public int getHealth()
+    {
+      return health;
+    }
+
+    public int getPower()
+    {
+      return power;
+    }
+
+    public string getName()
+    {
+      return name;
+    }
+
+    public void adjustPower(int mod)
+    {
+      power = power + mod;
+    }
+
+    public void adjustHealth(int mod)
+    {
+      health = health + mod;
+    }
+  }
+
+
+  }
+}
