54834920117b74e16d3bbc9cfba27e33d872b899
lckybob742@gmail.com <lckybob742@gmail.com@b4ff2f4a-5cd7-df60-c775-ff6b566891b7>
2010-10-25 01:32:06 +0000

    added getter and setter methods to Player

--- trunk/arrg/ARRG/Player.cs
+++ trunk/arrg/ARRG/Player.cs
@@ -7,11 +7,76 @@ namespace ARRG_Game
 {
     class Player
     {
+        private static int MAX_NUM_DIE = 3;
         private int health;
         private int mana;
+        private int gold;
+
+        
         private Die[] die;
+        private int numDie;
         private List<Monster> monsters; //Ava
         private List<Card> availableCards;
+
+        public Player()
+        {
+            health = 20;
+            mana = 10;
+            gold = 100;
+            die = new Die[MAX_NUM_DIE];
+            monsters = new List<Monster>();
+            availableCards = new List<Card>();
+            numDie = 0;
+        }
+
+
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        public int Mana
+        {
+            get { return mana; }
+            set { mana = value; }
+        }
+
+        internal Die[] Die
+        {
+            get { return die; }
+            set { die = value; }
+        }
+
+        internal List<Monster> Monsters
+        {
+            get { return monsters; }
+            set { monsters = value; }
+        }
+
+        internal List<Card> AvailableCards
+        {
+            get { return availableCards; }
+            set { availableCards = value; }
+        }
+
+        public int Gold
+        {
+            get { return gold; }
+            set { gold = value; }
+        }
+
+        public bool addDie(Die d)
+        {
+            if (numDie < 3)
+            {
+                die[numDie++] = d;
+                return true;
+            }
+            return false;
+        }
+
+
 
     }
 }
