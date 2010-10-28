b3753604c448d811b54c65dbf7ba67e255c4d966
lckybob742@gmail.com <lckybob742@gmail.com@b4ff2f4a-5cd7-df60-c775-ff6b566891b7>
2010-10-28 17:15:44 +0000

    Updated Submit button

---
+++ arrg/ARRG/Dragonkin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ARRG_Game
+{
+    public class Dragonkin : Monster
+    {
+        public Dragonkin(String name, int health, int power) { }
+    }
+}
