3d36f54162a1415491064b258c77dba1376e6d5e
lckybob742@gmail.com <lckybob742@gmail.com@b4ff2f4a-5cd7-df60-c775-ff6b566891b7>
2010-10-31 17:00:56 +0000

    added new models and fixed positioning issues with Models

--- trunk/arrg/ARRG/ARRG.cs
+++ trunk/arrg/ARRG/ARRG.cs
@@ -223,29 +223,29 @@ namespace ARRG_Game
 
 	private void CreateGround()
 	{
-	  GeometryNode groundNode = new GeometryNode("Ground");
+	  //GeometryNode groundNode = new GeometryNode("Ground");
 
 #if USE_ARTAG
 			groundNode.Model = new Box(85, 66, 0.1f);
 #else
-	  groundNode.Model = new Box(95, 59, 0.1f);
+	  //groundNode.Model = new Box(95, 59, 0.1f);
 #endif
 
 	  // Set this ground model to act as an occluder so that it appears transparent
-	  groundNode.IsOccluder = true;
+	  //groundNode.IsOccluder = true;
 
 	  // Make the ground model to receive shadow casted by other objects with
 	  // CastShadows set to true
-	  groundNode.Model.ReceiveShadows = true;
+	 // groundNode.Model.ReceiveShadows = true;
 
-	  Material groundMaterial = new Material();
-	  groundMaterial.Diffuse = Color.Black.ToVector4();
-	  groundMaterial.Specular = Color.White.ToVector4();
-	  groundMaterial.SpecularPower = 20;
+	  //Material groundMaterial = new Material();
+	  //groundMaterial.Diffuse = Color.Black.ToVector4();
+	  //groundMaterial.Specular = Color.White.ToVector4();
+	  //groundMaterial.SpecularPower = 20;
 
 	  //groundNode.Material = groundMaterial;
 
-	  groundMarkerNode.AddChild(groundNode);
+	  //groundMarkerNode.AddChild(groundNode);
 
 	  p = new Player(scene, 1, groundMarkerNode);
       p2 = new Player(scene, 2, groundMarkerNode);
