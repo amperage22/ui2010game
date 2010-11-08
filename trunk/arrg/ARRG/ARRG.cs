1cb5dbfe818a0c45523cea49d8e125fdd5aecef7
Snyper20@gmail.com <Snyper20@gmail.com@b4ff2f4a-5cd7-df60-c775-ff6b566891b7>
2010-11-08 05:29:22 +0000

    Update before commit damnit!

--- trunk/arrg/ARRG/ARRG.cs
+++ trunk/arrg/ARRG/ARRG.cs
@@ -256,8 +256,8 @@ namespace ARRG_Game
 
             monsters.Add(new MonsterBuilder(CreatureID.DRAGON1, CreatureType.DRAGONKIN, "Whelp", "dragon1", Content.Load<Texture2D>("Textures/inventory/dragon1"), 1, 3, true, 1, 0, scene));
             monsters.Add(new MonsterBuilder(CreatureID.DRAGON2, CreatureType.DRAGONKIN, "Drake", "dragon2", Content.Load<Texture2D>("Textures/inventory/dragon2"), 3, 5, false, 1, 0, scene));
-            monsters.Add(new MonsterBuilder(CreatureID.ESCAFLOWNE, CreatureType.DRAGONKIN, "UNKNOWN1", "escaflowne", Content.Load<Texture2D>("Textures/inventory/unknown"), 0, 0, false, 50, 0, scene));
-            monsters.Add(new MonsterBuilder(CreatureID.DRAGON3, CreatureType.DRAGONKIN, "UNKNOWN2", "dragon3", Content.Load<Texture2D>("Textures/inventory/unknown"), 0, 0, false, 50, 0, scene));
+            monsters.Add(new MonsterBuilder(CreatureID.ESCAFLOWNE, CreatureType.DRAGONKIN, "Escaflowne", "escaflowne", Content.Load<Texture2D>("Textures/inventory/escaflowne"), 0, 0, false, 50, 0, scene));
+            monsters.Add(new MonsterBuilder(CreatureID.DRAGON3, CreatureType.DRAGONKIN, "Dragon", "dragon3", Content.Load<Texture2D>("Textures/inventory/dragon3"), 0, 0, false, 50, 0, scene));
 
 
             //Set up the initial monsters for use after the talent screen has been submitted
