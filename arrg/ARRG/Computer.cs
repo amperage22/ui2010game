75c42d8db127f6609feea8ed32dd528c3c17e558
lckybob742@gmail.com <lckybob742@gmail.com@b4ff2f4a-5cd7-df60-c775-ff6b566891b7>
2010-12-02 03:16:00 +0000

    derp derp derp

---
+++ arrg/ARRG/Computer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using GoblinXNA.SceneGraph;
+
+namespace ARRG_Game
+{
+    class Computer: Player
+    {
+        public Computer(int playerNum, MarkerNode ground)
+            : base(playerNum, ground)
+        {
+        }
+
+        public override void updateAttack(Die[] die2)
+        {
+            foreach (Die d in die)
+            {
+                if (d != null && d.CurrentMonster != null)
+                {
+                    d.setWeakestEnemy(die2);
+                }
+            }
+        }
+    }
+}
